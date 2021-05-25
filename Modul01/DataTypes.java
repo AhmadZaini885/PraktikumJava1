ba010179292778ad17e5e9c7d6a928371b1630a6
ahmadzaini885 <ahmadzaini0703@gmail.com>
2021-05-25 16:24:10 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        String namaDepan = "Ahmad";
+        String namaBelakang = " Zaini";
+        int usia = 20;
+        int tergetTahunKuliah = 4;
+        double ipk = 3.999999;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+	}
+}
