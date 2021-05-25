2e39edf70c5a3eb886e7c16bf3dfefaa92b4358d
ahmadzaini885 <ahmadzaini0703@gmail.com>
2021-05-25 16:29:42 +0800

    output dengan messageDialog

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class DataTypes {
     public static void main(String[] args) {
 
@@ -17,5 +19,7 @@ public class DataTypes {
         System.out.println("IPK : " + ipk );
         System.out.println("Nilai PBO : " + nilaiAbjad);
         System.out.println("Tampan : " + tampan);
+
+	JOptionPane.showConfirmDialog(null,"hai " + namaDepan + namaBelakang) ;
 	}
 }
