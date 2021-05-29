a790fec6c9315d0722844143fe2fcb8ddac164ce
ahmadzaini885 <ahmadzaini0703@gmail.com>
2021-05-29 21:19:11 +0800

    mahasiswa simpleDateFormat.format

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -26,6 +26,6 @@ public class MahasiswaBeraksi {
 
         System.out.println("Nama  \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
     }
 }
