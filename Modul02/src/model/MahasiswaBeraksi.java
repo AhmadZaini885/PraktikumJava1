d3fefb7f671046de13f14e64ea3b9728951904a4
ahmadzaini885 <ahmadzaini0703@gmail.com>
2021-05-29 21:16:56 +0800

    mahasiswa simpleDateFormat

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "16630511";
         mahasiswa.nama = "Ahmad Zaini";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("07-03-2001");
 
         System.out.println("Nama  \t\t\t : " + mahasiswa.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa.npm);
@@ -15,8 +21,8 @@ public class MahasiswaBeraksi {
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19630001";
-        mahasiswa2.npm = " Galung";
-        mahasiswa2.tanggalLahir = new Date();
+        mahasiswa2.nama = "Galung";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("05-02-2000");
 
         System.out.println("Nama  \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
